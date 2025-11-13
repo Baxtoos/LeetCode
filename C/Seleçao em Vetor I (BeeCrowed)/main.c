39c23a0a5f3336d5353907a25767b439866de1ca
Sergio Bastos <giftc444@gmail.com>
2025-11-12 22:28:24 -0300

    Conclussão do problema do BeeCrowed

--- C/Seleçao em Vetor I (BeeCrowed)/main.c
+++ C/Seleçao em Vetor I (BeeCrowed)/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
-#include <time.h>
+// #include <time.h>
 
-int numeros_aleatorios() {
-  int A[100], i;
-  srand(time(NULL));
+// int numeros_aleatorios() {
+//   int A[100], i;
+//   srand(time(NULL));
 
-  for (int i = 0; i < 100; i++) {
-    int numero_aleatorio_completo = rand();
-    A[i] = numero_aleatorio_completo;
-  }
-  printf("%d", A);
-}
+//   for (int i = 0; i < 100; i++) {
+//     int numero_aleatorio_completo = rand();
+//     A[i] = numero_aleatorio_completo;
+//   }
+//   printf("%d", A);
+// }
 
 int main() {
-  numeros_aleatorios();
+  double A[100];
+  int i;
+
+  for (i = 0; i < 100; i++) {
+    scanf("%lf", &A[i]);
+  }
+
+  for (i = 0; i < 100; i++) {
+
+    if (A[i] <= 10.0) {
+      printf("A[%d] = %.1lf\n", i, A[i]);
+    }
+  }
+  
+  return 0;
 }
